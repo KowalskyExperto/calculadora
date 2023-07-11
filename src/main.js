34699acc9d0096f86b3e8a69d6b68109d2a6d7b6
KowalskyExperto <kowalskyexperto@gmail.com>
2023-07-10 23:36:40 -0600

    Numeros funcionando
    Y el punto solo se coloca una vez.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,93 @@
 const buttonPercent = document.getElementById("button-percent");
-const buttonSeven = document.getElementById("button-seven");
+const buttonCE = document.getElementById("button-ce")
+const button1 = document.getElementById("button-1");
+const button2 = document.getElementById("button-2");
+const button3 = document.getElementById("button-3");
+const button4 = document.getElementById("button-4");
+const button5 = document.getElementById("button-5");
+const button6 = document.getElementById("button-6");
+const button7 = document.getElementById("button-7");
+const button8 = document.getElementById("button-8");
+const button9 = document.getElementById("button-9");
+const button0 = document.getElementById("button-0");
+const buttonDot = document.getElementById("button-dot");
 
-buttonSeven.addEventListener(
+
+button1.addEventListener(
+    "click"
+    ,() => addTextToSecondLine("1")
+);
+
+button2.addEventListener(
+    "click"
+    ,() => addTextToSecondLine("2")
+);
+
+button3.addEventListener(
+    "click"
+    ,() => addTextToSecondLine("3")
+);
+
+button4.addEventListener(
+    "click"
+    ,() => addTextToSecondLine("4")
+);
+
+button5.addEventListener(
+    "click"
+    ,() => addTextToSecondLine("5")
+);
+
+button6.addEventListener(
+    "click"
+    ,() => addTextToSecondLine("6")
+);
+
+button7.addEventListener(
     "click"
     ,() => addTextToSecondLine("7")
-)
+);
+
+button8.addEventListener(
+    "click"
+    ,() => addTextToSecondLine("8")
+);
+
+button9.addEventListener(
+    "click"
+    ,() => addTextToSecondLine("9")
+);
+
+button0.addEventListener(
+    "click"
+    ,() => addTextToSecondLine("0")
+);
+
+buttonDot.addEventListener(
+    "click"
+    ,() => addTextToSecondLine(".")
+);
+
 buttonPercent.addEventListener(
     "click"
     ,() => addTextToSecondLine("%")
 );
 
-const addTextToSecondLine = (word) => {
+buttonCE.addEventListener(
+    "click"
+    ,() => addTextToFirstLine("CE")
+);
+
+const addTextToSecondLine = function (word) {
     const secondLine = document.getElementById("second-line");
-    const oldText = secondLine.textContent;
-    secondLine.textContent = oldText + word
+    var oldText = secondLine.textContent;
+    if(word === '.' && oldText.includes('.'))
+        word = '';
+    secondLine.textContent = oldText + word;
+};
+
+const addTextToFirstLine = function (word) {
+    const firstLine = document.getElementById("first-line");
+    const oldText = firstLine.textContent;
+    firstLine.textContent = oldText + word;
 };
